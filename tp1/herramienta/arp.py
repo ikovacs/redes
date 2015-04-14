05d0f75ceca4aaa098fa9b9fdff2eac217b86fa8
Ignacio Kovacs <ignacio.k91@gmail.com>
2015-04-14 18:53:42 -0300

    entropía arp

--- tp1/herramienta/arp.py
+++ tp1/herramienta/arp.py
@@ -10,11 +10,15 @@ TYPE_ARP = 0x806
 ARP_REQUEST = 1
 ARP_REPLY = 2
 
+RLY = 0
+REQ = 1
+
 eth_count = 0
 arp_count = 0
 
 eth = dict()
 arp = dict()
+arp2 = dict()
 
 def eth_entropy():
 	global eth, eth_count
@@ -29,7 +33,24 @@ def eth_entropy():
 					entropy += p * -(log(p)/log(2))
 	return entropy
 
-def packet_captured(packet):
+def arp_entropy():
+	global arp, arp_count
+	entropy = .0
+	for psrc in arp:
+		for pdst in arp[psrc]:
+			c = arp[psrc][pdst][RLY]
+			if c > 0:
+				p = (float(c) / float(arp_count))
+				entropy += p * -(log(p)/log(2))
+
+			c = arp[psrc][pdst][REQ]
+			if c > 0:
+				p = (float(c) / float(arp_count))
+				entropy += p * -(log(p)/log(2))
+
+	return entropy
+
+def eth_packet(packet):
 	global eth, eth_count
 
 	eth_count += 1
@@ -52,7 +73,38 @@ def packet_captured(packet):
 	eth_ent = eth_entropy()
 
 	print 'Ethernet Entropy: {}'.format(eth_ent)
-		
-		
+
+def arp_packet(packet):
+	global arp, arp_count
+
+	arp_count += 1
+
+	psrc = packet[ARP].psrc
+	pdst = packet[ARP].pdst
+	request = true if packet[ARP].op == ARP_REQUEST else false
+
+	if psrc not in arp:
+		arp[psrc] = dict()
+
+	if pdst not in arp[psrc]:
+		arp[psrc][pdst] = [0, 0]
+
+	if request:
+		arp[psrc][pdst][REQ] += 1
+	else:
+		arp[psrc][pdst][RLY] += 1
+
+	arp_ent = arp_entropy()
+
+	print 'ARP Entropy: {}'.format(arp_ent)
+
+
+def packet_captured(packet):
+	try:
+		eth_packet(packet)
+		if packet[Ether].type == TYPE_ARP:
+			arp_packet(packet)
+	except Exception, e:
+		print e
 
 sniff(filter='', prn=packet_captured)
