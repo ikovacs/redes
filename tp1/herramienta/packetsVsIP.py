bc29fa6c796867bb818e309b692fd7d2f83a5a8f
marisol <mreartes@devartis.com>
2015-04-26 18:56:55 -0300

    graficos, faltan los de la captura del laburo de eze

--- tp1/herramienta/packetsVsIP.py
+++ tp1/herramienta/packetsVsIP.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python2
 
 import sys
+import math
 
+entropy = 8.5144861881
 hist = open(sys.argv[1], 'w+')
 
 temp = dict()
+total = 0
 
 for line in sys.stdin:
 	values = line.strip().split()
 	if values[0] == 'arp':
+		total += 1
 		key = values[1]
 		if not key in temp.keys():
 			temp[key] = 1
 		else:
 			temp[key] += 1
-	
+
 for key, value in temp.items():
-	hist.write(key + ' ' + str(value) + '\n')
+	p = float(value)/float(total)
+	i = - math.log(p,2)
+	hist.write(key + ' ' + str(i) + '\n')
 hist.close()
